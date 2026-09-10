e8d1d389f844e71fef7ac4e467b2dac17d6bfe74
Bizimana Eric <ericbizimana111@gmail.com>
2026-09-10 20:35:25 +0200

    first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -374,9 +374,15 @@ public class HelloWorld {
 
         System.out.printf("Input the first number:");
         double number1 = scanner.nextDouble();
+        scanner.nextLine()
 
         System.out.printf(("Input the second number:"));
         double number2 = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("What operation do you want to perform? ");
+        String operation = scanner.nextLine();
+
 
         // System.out.println(number1);
         // System.out.println(number2);
