27f776e68545c260d1f676891191862f51fad3e9
Bizimana Eric <ericbizimana111@gmail.com>
2026-09-10 20:45:24 +0200

    added if condition

--- HelloWorld.java
+++ HelloWorld.java
@@ -374,16 +374,29 @@ public class HelloWorld {
 
         System.out.printf("Input the first number:");
         double number1 = scanner.nextDouble();
-        scanner.nextLine()
-
+        scanner.nextLine();
+       
         System.out.printf(("Input the second number:"));
         double number2 = scanner.nextDouble();
         scanner.nextLine();
+       
 
         System.out.print("What operation do you want to perform? ");
         String operation = scanner.nextLine();
 
 
+        if (operation.equals("sum")){
+
+            System.out.printf("%f + %f = %f ", number1 , number2 , number1 + number2);
+
+        }else if (operation.equals("sub")){
+
+            System.out.printf("%f - %f = %f ", number1 ,number2 , number1 -  number2);
+        }
+
+
+
+        
         // System.out.println(number1);
         // System.out.println(number2);
        
