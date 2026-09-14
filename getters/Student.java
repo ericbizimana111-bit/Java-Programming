085de8bfa17bdfca466897853762f05d44cf5969
Bizimana Eric <ericbizimana111@gmail.com>
2026-09-14 14:37:11 +0200

    added code

--- getters/Student.java
+++ getters/Student.java
@@ -1,3 +1,4 @@
+//package getters;
 // public class Student {
 //     private String firstName;
 //     private String lastName;
@@ -38,14 +39,11 @@
 // //any pulbic method can access them private varibles int eh class we have getter and setters //private varibles can be accessible only in the same class;
 //getter and setters are special method that are used to get and set the values 
 
-
-
-
-
-
 //====================== using the getters to access the private varibles values in other files not in the same class ================= //4
 // ========================== getting the access to the private varibles in the class that are only accessible in the class in other file ================== //
+
 public class Student {
+
     private String firstName;
     private String lastName;
     private String email;
