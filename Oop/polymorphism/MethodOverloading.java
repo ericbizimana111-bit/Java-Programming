ee1ce6e15ef200818543e14047828c5e6458e625
Bizimana Eric <ericbizimana111@gmail.com>
2026-09-17 14:17:00 +0200

    changed file location

---
+++ Oop/polymorphism/MethodOverloading.java
@@ -0,0 +1,3 @@
+package Oop.polymorphism;
+//Method Overloading = compile-time polymorphism 
+//Overloading == means having multiple methods with the same name but different parameters in the same class 
