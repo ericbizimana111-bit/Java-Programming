e90b8ef6cc380741f933c75f1fd42435da0a437f
Bizimana Eric <ericbizimana111@gmail.com>
2026-09-17 15:09:38 +0200

    added code

---
+++ Oop/polymorphism/MethodOverLoadingMain.java
@@ -0,0 +1,10 @@
+package Oop.polymorphism;
+
+public class MethodOverLoadingMain {
+  public static void main(String[] args) {
+    MethodOverloading calculator = new MethodOverloading();
+    calculator.add(10,10);
+    calculator.add(10, 10);
+    calculator.add(10, 10);
+  }
+}
--- Oop/polymorphism/MethodOverloading.java
+++ Oop/polymorphism/MethodOverloading.java
@@ -1,11 +1,20 @@
 package Oop.polymorphism;
 //Method Overloading = compile-time polymorphism 
+
 //Overloading == means having multiple methods with the same name but different parameters in the same class 
 
 public class MethodOverloading {
 
-  int add (int a , int b){
-    
+int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
   }
-  
-}
