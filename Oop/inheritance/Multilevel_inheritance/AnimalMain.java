f51cdd086d69c370b2747653a2b628f619caff7f
Bizimana Eric <ericbizimana111@gmail.com>
2026-09-18 22:09:41 +0200

    added code

--- Oop/inheritance/Multilevel_inheritance/AnimalMain.java
+++ Oop/inheritance/Multilevel_inheritance/AnimalMain.java
@@ -2,6 +2,21 @@ package Oop.inheritance.Multilevel_inheritance;
 
 public class AnimalMain {
   public static void main(String[] args) {
+    Dog dog = new Dog();
+
+    System.out.println("============For the Dog class ===============");
+
+    dog.bark();
+    dog.eat();
+    dog.breath();
+
+
+
+    System.out.println("============For the Mammal class==============");
+
+    Mammal mammal = new Mammal();
+    mammal.breath();
+    mammal.eat();
 
 
 
