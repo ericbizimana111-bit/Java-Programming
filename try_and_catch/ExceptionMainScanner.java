8642c08436efbb6af5d8f6097f8517eb5dca5643
Bizimana Eric <ericbizimana111@gmail.com>
2026-09-23 10:10:32 +0200

    added code

--- try_and_catch/ExceptionMainScanner.java
+++ try_and_catch/ExceptionMainScanner.java
@@ -5,14 +5,26 @@ import java.util.Scanner;
 public class ExceptionMainScanner {
   public static void main(String[] args) {
 
+    //c is a resource so it have to be closed
     Scanner c = new Scanner(System.in);
+
     double amount;
     double balance = 5000;
 
     System.out.println("Enter Amount");
     amount = c.nextDouble();
-    
-    
+
+    try {
+      if (amount > balance)
+        throw new WithdrawException("Insuficient amount");
+      System.out.println("success");
+
+    } catch (WithdrawException e) {
+      System.out.println(e);
+
+    }finally{
+      c.close();
+    }
 
   }
 }
--- try_and_catch/WithdrawException.java
+++ try_and_catch/WithdrawException.java
@@ -1,10 +1,9 @@
 package try_and_catch;
 
-
 //runtime exception
 //super call the constructor exception  constructor formthe object class form the exception class 
-public class WithdrawException extends Exception{
-  public WithdrawException(String message){
-super("insufficient amount");
+public class WithdrawException extends Exception {
+  public WithdrawException(String message) {
+    super("insufficient amount");
   }
 }
