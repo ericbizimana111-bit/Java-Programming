d086004e97acde1c447a13327957c25d8098f3ab
Bizimana Eric <ericbizimana111@gmail.com>
2026-09-23 09:54:30 +0200

    added codes

--- try_and_catch/WithdrawException.java
+++ try_and_catch/WithdrawException.java
@@ -1,7 +1,10 @@
 package try_and_catch;
 
+
+//runtime exception
+//super call the constructor exception  constructor formthe object class form the exception class 
 public class WithdrawException extends Exception{
   public WithdrawException(String message){
-    
+super("insufficient amount");
   }
 }
